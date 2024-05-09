2f1ce7237610f07a6c74fd424d1e912f2ad63968
Curtis Whitley <restday7@gmail.com>
2024-05-09 15:11:37 -0400

    interim checkin

--- src/cpu.v
+++ src/cpu.v
@@ -76,22 +76,22 @@ reg [31:0] reg_ir;          // Instruction
 // 6502 Address modes
 
 typedef enum {
-    ABS,            // Absolute a
-    ABS_INX_IND,    // Absolute Indexed Indirect (a,x)
-    ABS_INX_X,      // Absolute Indexed with X a,x
-    ABS_INX_Y,      // Absolute Indexed with Y a, y
-    ABS_IND,        // Absolute Indirect (a)
-    ACC,            // Accumulator A
-    IMM,            // Immediate Addressing #
-    IMP,            // Implied i
-    PCR,            // Program Counter Relative r
-    STK,            // Stack s
-    ZPG,            // Zero Page zp
-    ZPG_INX_IND,    // Zero Page Indexed Indirect (zp,x)
-    ZPG_INX_X,      // Zero Page Indexed with X zp,x
-    ZPG_INX_Y,      // Zero Page Indexed with Y zp, y
-    ZPG_IND,        // Zero Page Indirect (zp)
-    ZPG_IND_INX_Y   // Zero Page Indirect Indexed with Y (zp), y
+    ABS,    // Absolute a
+    AII,    // Absolute Indexed Indirect (a,x)
+    AIX,    // Absolute Indexed with X a,x
+    AIY,    // Absolute Indexed with Y a,y
+    AIA,    // Absolute Indirect (a)
+    ACC,    // Accumulator A
+    IMM,    // Immediate Addressing #
+    IMP,    // Implied i
+    PCR,    // Program Counter Relative r
+    STK,    // Stack s
+    ZPG,    // Zero Page zp
+    ZII,    // Zero Page Indexed Indirect (zp,x)
+    ZIX,    // Zero Page Indexed with X zp,x
+    ZIY,    // Zero Page Indexed with Y zp,y
+    ZPI,    // Zero Page Indirect (zp)
+    ZIIY    // Zero Page Indirect Indexed with Y (zp),y
 } AddressMode;
 
 typedef enum {
@@ -164,22 +164,26 @@ typedef enum {
     TXA,
     TXS,
     TYA,
-    WAI,
+    WAI
 } Operation;
 
 // 6502 Instructions
 
 // Processing registers
 
-typedef struct {
+typedef struct packed {
+    Operation       operation;
     AddressMode     address_mode;
-    logic [2:0]     data_reg_bank;
-    logic [3:0]     data_reg_index;
 } DecodedInstruction;
 
 reg [2:0] reg_stage;
-reg DataWidth reg_data_width;
-reg DecodedInstruction reg_instr;
+wire DecodedInstruction tmp_instr;
+wire DataWidth tmp_data_width;
+wire [2:0] tmp_src_bank;
+wire [3:0] tmp_src_index;
+wire [2:0] tmp_dst_bank;
+wire [3:0] tmp_dst_index;
+wire [2:0] tmp_which;
 
 always @(posedge i_rst or posedge i_clk) begin
     if (i_rst) begin
@@ -197,8 +201,144 @@ always @(posedge i_rst or posedge i_clk) begin
         case (reg_stage)
             0: begin
                     // Decode instruction
+                    tmp_data_width = DATA_WIDTH_8;
+                    tmp_src_bank = 0;
+                    tmp_src_index = 0;
+                    tmp_dst_bank = 0;
+                    tmp_dst_index = 0;
+                    tmp_which = 0;
+
+                    // Determine operation
                     case (IR0)
-                        8'h00: 
+                        8'h: tmp_inst = `{ ADC, };
+
+                        8'h22: tmp_inst = `{ AND, ZII };
+                        8'h25: tmp_inst = `{ AND, ZPG };
+                        8'h29: tmp_inst = `{ AND, IMM };
+                        8'h2D: tmp_inst = `{ AND, ABS };
+                        8'h32: tmp_inst = `{ AND, ZIIY };
+                        8'h33: tmp_inst = `{ AND, ZPI };
+                        8'h35: tmp_inst = `{ AND, ZIX };
+                        8'h39: tmp_inst = `{ AND, AIY };
+                        8'h3D: tmp_inst = `{ AND, AIX };
+
+                        8'h06: tmp_inst = `{ ASL, ZPG };
+                        8'h0A: tmp_inst = `{ ASL, ACC };
+                        8'h0E: tmp_inst = `{ ASL, ABS };
+                        8'h16: tmp_inst = `{ ASL, ZIX };
+
+                        8'hF0: tmp_inst = `{ BEQ, IMM };
+                        8'h: tmp_inst = `{ BIT, };
+
+                        8'h0F, 8'h1F, 8'h2F, 8'h3F,
+                        8'h4F, 8'h5F, 8'h6F, 8'h7F:
+                            begin
+                                tmp_inst = `{ BBR, PCR };
+                                tmp_which = reg_ir[6:4];
+                            end
+
+                        8'h8F, 8'h8F, 8'hAF, 8'hBF,
+                        8'hCF, 8'hDF, 8'hEF, 8'hFF:
+                            begin
+                                tmp_inst = `{ BBS, PCR };
+                                tmp_which = reg_ir[6:4];
+                            end
+
+                        8'h90: tmp_inst = `{ BCC, PCR };
+                        8'hB0: tmp_inst = `{ BCS, PCR };
+                        8'h30: tmp_inst = `{ BMI, PCR };
+                        8'hD0: tmp_inst = `{ BNE, PCR };
+                        8'h10: tmp_inst = `{ BPL, PCR };
+                        8'h80: tmp_inst = `{ BRA, PCR };
+                        8'h00: tmp_inst = `{ BRK, STK };
+                        8'h50: tmp_inst = `{ BVC, PCR };
+                        8'h70: tmp_inst = `{ BVS, PCR };
+                        8'h18: tmp_inst = `{ CLC, IMP };
+                        8'hD8: tmp_inst = `{ CLD, IMP };
+                        8'h58: tmp_inst = `{ CLI, IMP };
+                        8'hB8: tmp_inst = `{ CLV, IMP };
+                        8'h: tmp_inst = `{ CMP, };
+                        8'hE0: tmp_inst = `{ CPX, IMM };
+                        8'hC0: tmp_inst = `{ CPY, IMM };
+
+                        8'h3A: tmp_inst = `{ DEC, ACC };
+                        8'hCA: tmp_inst = `{ DEX, IMP };
+                        8'h88: tmp_inst = `{ DEY, IMP };
+
+                        8'h: tmp_inst = `{ EOR, };
+
+                        8'1Ah: tmp_inst = `{ INC, ACC };
+                        8'hE8: tmp_inst = `{ INX, IMP };
+                        8'hC8: tmp_inst = `{ INY, IMP };
+
+                        8'h: tmp_inst = `{ JMP, };
+                        8'h20: tmp_inst = `{ JSR, ABS };
+
+                        8'h: tmp_inst = `{ LDA, };
+                        8'h: tmp_inst = `{ LDX, };
+                        8'hA0: tmp_inst = `{ LDY, IMM };
+
+                        8'h4A: tmp_inst = `{ LSR, ACC };
+
+                        8'hEA: tmp_inst = `{ NOP, IMP };
+
+                        8'h01: tmp_inst = `{ ORA, ZIX };
+                        8'h05: tmp_inst = `{ ORA, ZPG };
+                        8'h09: tmp_inst = `{ ORA, IMM };
+                        8'h0D: tmp_inst = `{ ORA, ABS };
+                        8'h11: tmp_inst = `{ ORA, ZIIY };
+                        8'h12: tmp_inst = `{ ORA, ZPI };
+                        8'h15: tmp_inst = `{ ORA, ZIX };
+
+                        8'h48: tmp_inst = `{ PHA, STK };
+                        8'h08: tmp_inst = `{ PHP, STK };
+                        8'hDA: tmp_inst = `{ PHX, STK };
+                        8'h5A: tmp_inst = `{ PHY, STK };
+                        8'h68: tmp_inst = `{ PLA, STK };
+                        8'h28: tmp_inst = `{ PLP, STK };
+                        8'hFA: tmp_inst = `{ PLX, STK };
+                        8'h7A: tmp_inst = `{ PLY, STK };
+
+                        8'h07, 8'h17, 8'h27, 8'h37,
+                        8'h47, 8'h57, 8'h67, 8'h77:
+                            begin
+                                tmp_inst = `{ RMB, ZPG };
+                                tmp_which = reg_ir[6:4];
+                            end
+
+                        8'h2A: tmp_inst = `{ ROL, ACC };
+                        8'h6A: tmp_inst = `{ ROR, ACC };
+                        8'h40: tmp_inst = `{ RTI, STK };
+                        8'h60: tmp_inst = `{ RTS, STK };
+                        8'h: tmp_inst = `{ SBC, };
+                        8'h38: tmp_inst = `{ SEC, IMP };
+                        8'hF8: tmp_inst = `{ SED, IMP };
+                        8'h78: tmp_inst = `{ SEI, IMP };
+
+                        8'h87, 8'h97, 8'hA7, 8'hB7,
+                        8'hC7, 8'hD7, 8'hE7, 8'hF7:
+                            begin
+                                tmp_inst = `{ SMB, ZPG };
+                                tmp_which = reg_ir[6:4];
+                            end
+
+                        8'h: tmp_inst = `{ STA, };
+                        8'hDB: tmp_inst = `{ STP, IMP };
+                        8'h: tmp_inst = `{ STX, };
+                        8'h: tmp_inst = `{ STY, };
+                        8'h: tmp_inst = `{ STZ, };
+                        8'hAA: tmp_inst = `{ TAX, IMP };
+                        8'hA8: tmp_inst = `{ TAY, IMP };
+                        8'h14: tmp_inst = `{ TRB, ZPG };
+
+                        8'h04: tmp_inst = `{ TSB, ZPG };
+                        8'h0C: tmp_inst = `{ TSB, ABS };
+
+                        8'hBA: tmp_inst = `{ TSX, IMP };
+                        8'h8A: tmp_inst = `{ TXA, IMP };
+                        8'h9A: tmp_inst = `{ TXS, IMP };
+                        8'h98: tmp_inst = `{ TYA, IMP };
+                        8'hCB: tmp_inst = `{ WAI, IMP }; 
                     endcase
                 end
         endcase
