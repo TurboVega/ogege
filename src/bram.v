8f55d101d935c3fe02d2d1627a425209709edbbf
Curtis Whitley <restday7@gmail.com>
2024-06-18 13:54:50 -0400

    formatting

--- src/bram.v
+++ src/bram.v
@@ -10,16 +10,16 @@
 `default_nettype none
 
 module bram_64kb(
-        input wire wea,               // write enable A
-        input wire web,               // write enable B
-        input wire clka,              // clock A
-        input wire clkb,              // clock B
-        input wire [7:0] dia,         // data in A
-        input wire [7:0] dib,         // data in B
-        input wire [15:0] addra,       // address A
-        input wire [15:0] addrb,       // address B
-        output reg [7:0] doa,         // data out A
-        output reg [7:0] dob          // data out B
+        input wire wea,             // write enable A
+        input wire web,             // write enable B
+        input wire clka,            // clock A
+        input wire clkb,            // clock B
+        input wire [7:0] dia,       // data in A
+        input wire [7:0] dib,       // data in B
+        input wire [15:0] addra,    // address A
+        input wire [15:0] addrb,    // address B
+        output reg [7:0] doa,       // data out A
+        output reg [7:0] dob        // data out B
     );
 
     reg [7:0] bram [0:65535];
