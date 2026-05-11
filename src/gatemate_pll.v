cb96c412b0863da150befe617efaf67eb52bb1a6
Curtis Whitley <restday7@gmail.com>
2026-05-11 12:42:19 -0400

    fix typos in new PLL code

--- src/gatemate_pll.v
+++ src/gatemate_pll.v
@@ -12,14 +12,15 @@ module clock_gen_50_25 (
     // Ratio: 10 * (201 / 40) = 50.250 MHz
     CC_PLL #(
         .REF_CLK("10.0"),
-        .P_DIV(40), // N = 40
-        .M_MULT(201), // M = 201
-        .S_DIV(0) // S = 0 (2^0 = 1)
+        .OUT_CLK(50.250),
+        .LOW_JITTER(1),
+        .CI_FILTER_CONST(2),
+        .CP_FILTER_CONST(4)
     ) pll_inst (
         .CLK_REF(clk_osc),
         .CLK_FEEDBACK(clk_fb),
-        .CLK_OUT(clk_50_25),
-        .CLK_LOCK(pll_lock)
+        .CLK0(clk_50_25),
+        .USR_PLL_LOCKED_STDY(pll_lock)
     );
 
     assign clk_fb = clk_50_25;
@@ -36,7 +37,7 @@ module clock_gen_50_25 (
     // Global Buffer for the divided clock
     CC_BUFG bufg_25 (
         .I(r_clk_25),
-        .Y(clk_25_12)
+        .O(clk_25_12)
     );
 
 wire sys_rst_n;
